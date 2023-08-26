68d854c781deaec4f5883231185eb917bed46474
Rodrigo MC <rodrigo.verde@hotmail.com>
2023-08-26 12:28:03 +0100

    Hello world, multiple languages

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
-print("Hello World!")
+import os
+
+current_language = os.getenv("LANG")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"
+
+print(msg)
