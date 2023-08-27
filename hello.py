867d8e19ccc3ef556f357898b64a90aa505c7b7a
Rodrigo MC <rodrigo.verde@hotmail.com>
2023-08-27 16:31:26 +0100

    Hello world, multiple languages!

--- hello.py
+++ hello.py
@@ -9,5 +9,9 @@ if current_language == "pt_BR":
     msg = "Ola, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
 
 print(msg)
